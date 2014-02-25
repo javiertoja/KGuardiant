099e549e7424c55bdd6da0c574ea466ec2427b76
Javier Toja <javi.t.fic@gmail.com>
2014-02-25 20:03:50 +0100

    Versión Inicial de la consola que interpreta las opciones del programa, actualmente solo soportamos 2 opciones que son las siguientes :
    	-1. -Opciones inicial actualment vacía.
    	-q. -Finaliza el programa con código 0.
    
    Se añaden scripts de CMake y Make para la consutrción automática dle programa.

---
+++ KGuardiant_console-build/CMakeFiles/KGuardiant_console.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/KGuardiant_console.dir/main.cpp.o"
+  "KGuardiant_console.pdb"
+  "KGuardiant_console"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/KGuardiant_console.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
