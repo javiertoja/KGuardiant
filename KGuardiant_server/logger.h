18814a92be4f45ecb9d783d2209351dbca58f443
Javier Toja <javi.t.fic@gmail.com>
2014-05-13 20:42:13 +0200

    Añadese unha nova clase encargada de xestionar os logs do sistema, usa variables de configuración para saber onde ter que gardar o log

---
+++ KGuardiant_server/logger.h
@@ -0,0 +1,35 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <QObject>
+#include <QDir>
+#include <QString>
+#include <QFile>
+#include <QDateTime>
+#include <QDebug>
+#include "configuracion.h"
+#include "constants.h"
+
+class Logger : public QObject
+{
+    Q_OBJECT
+public:
+    static Logger* Instance();
+
+    void log(QString logData);
+    void logImage(QImage* logImage);
+
+signals:
+    
+public slots:
+
+private:
+    QDir* logDir;
+    QFile* logFile;
+
+    static Logger* pinstance;
+protected:
+    explicit Logger(QObject *parent = 0);
+};
+
+#endif // LOGGER_H
