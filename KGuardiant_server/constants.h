07c5695256f568d7033b1c23ed1c9d2f6798c427
Javier Toja <javi.t.fic@gmail.com>
2014-05-13 20:43:33 +0200

    Creanse novas constantes para xestionar excepcions

--- KGuardiant_server/constants.h
+++ KGuardiant_server/constants.h
@@ -2,7 +2,8 @@
 #define CONSTANTS_H
 
 #define EX_NO_INPUT 'NO_INPUT'
-#define EX_NO_WRITEABLE 'NO_WRITEABLE'
+#define EX_NO_WRITE 'NO_WRITE'
 #define EX_NO_DATA 'NO_DATA'
+#define EX_NO_READABLE 'NO_READ'
 
 #endif // CONSTANTS_H
