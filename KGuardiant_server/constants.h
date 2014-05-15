026015d5e17bb0007d95b59173865e0fdfab4130
Javier Toja <javi.t.fic@gmail.com>
2014-05-15 19:51:04 +0200

    Creanse novas constantes necesarias para o a execición dos algoritmos de detección de movemento.

--- KGuardiant_server/constants.h
+++ KGuardiant_server/constants.h
@@ -5,5 +5,8 @@
 #define EX_NO_WRITE 'NO_WRITE'
 #define EX_NO_DATA 'NO_DATA'
 #define EX_NO_READABLE 'NO_READ'
+#define EX_ER_MVDETEC 'EX_ER_MVD'
+#define KINNECT '1'
+#define WEBCAM '0'
 
 #endif // CONSTANTS_H
